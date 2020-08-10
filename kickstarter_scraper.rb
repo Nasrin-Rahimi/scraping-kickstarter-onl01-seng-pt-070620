4e1fb9ab52a9f1298bae8faacd6bbaeb51ee04d6
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-10 11:53:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   project =  kickstarter.css("li.project.grid_4").first
-  title = project.css("h2.bbcard_name strong a")
+  title = project.css("h2.bbcard_name strong a").text
   binding.pry
 end
